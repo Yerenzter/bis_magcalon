171b4771e77515c922a7a3d39f0b1c1f73137909
Yerenzter <yerenzter@yandex.com>
2024-06-30 05:36:25 +0800

    -- changing <AddResident /> directory

--- src/components/Pages/ManageResidents.jsx
+++ src/components/Pages/ManageResidents.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-import AddResident from './components/AddResident.jsx';
+import AddResident from './Actions/AddResident.jsx';
 
 import {
   Button,
